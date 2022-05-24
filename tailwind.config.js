2e2de811cb240e9b68e9c5d526e6dfc68bb6a3a8
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2022-05-24 12:17:40 -0300

    fix: adjust media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      mobile: { "max": "425px" },
+      laptop: "1024px",
+      tablet: "768px",
+      desktop: "1280px",
+    },
     extend: {},
   },
   plugins: [],
